5ea357e8205bd541d105d6bb0158e38f8d416256
flpliao <flpliao@gmail.com>
2025-03-26 15:31:53 +0800

    更新配置和前端頁面

--- server.js
+++ server.js
@@ -51,4 +51,4 @@ app.post('/api/data', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
